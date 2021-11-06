a06957b788388fc3cf2bccf19cbfebd35318e6d9
Rishab-Shah <rishabshah1994@gmail.com>
2021-11-05 18:08:03 -0600

    updated with wifi related configuration. R-pi boot successful post addition of wifi support

--- build.sh
+++ build.sh
@@ -10,11 +10,32 @@ git submodule update
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env
 
+#########################################################################
+# Check if the support is present in local.conf file
+#########################################################################
+# Add target specification
 CONFLINE="MACHINE = \"raspberrypi3\""
-
 cat conf/local.conf | grep "${CONFLINE}" > /dev/null
 local_conf_info=$?
 
+# Add Wifi support
+DISTRO_F="DISTRO_FEATURES_append = \"wifi\""
+cat conf/local.conf | grep "${DISTRO_F}" > /dev/null
+local_distro_info=$?
+
+# Add firmware aupport
+IMAGE_ADD="IMAGE_INSTALL_append = \"linux-firmware-rpidistro-bcm43430 v4l-utils python3 ntp wpa-supplicant\""
+cat conf/local.conf | grep "${IMAGE_ADD}" > /dev/null
+local_imgadd_info=$?
+
+# Add ssh support
+IMAGE_F="IMAGE_FEATURES += \"ssh-server-openssh\""
+cat conf/local.conf | grep "${IMAGE_F}" > /dev/null
+local_imgf_info=$?
+
+##########################################################################
+# Add if the support is missing in the local.conf file
+##########################################################################
 if [ $local_conf_info -ne 0 ];then
 	echo "Append ${CONFLINE} in the local.conf file"
 	echo ${CONFLINE} >> conf/local.conf
@@ -23,8 +44,35 @@ else
 	echo "${CONFLINE} already exists in the local.conf file"
 fi
 
-###########################################################################
 
+if [ $local_distro_info -ne 0 ];then
+        echo "Append ${DISTRO_F} in the local.conf file"
+        echo ${DISTRO_F} >> conf/local.conf
+        
+else
+        echo "${DISTRO_F} already exists in the local.conf file"
+fi
+
+if [ $local_imgadd_info -ne 0 ];then
+        echo "Append ${IMAGE_ADD} in the local.conf file"
+        echo ${IMAGE_ADD} >> conf/local.conf
+        
+else
+        echo "${IMAGE_ADD} already exists in the local.conf file"
+fi
+
+if [ $local_imgf_info -ne 0 ];then
+        echo "Append ${IMAGE_F} in the local.conf file"
+        echo ${IMAGE_F} >> conf/local.conf
+        
+else
+        echo "${IMAGE_F} already exists in the local.conf file"
+fi
+
+###########################################################################
+# Baking the layers as per update in the local.conf file based upon if the
+# meta-layers are present or not already
+###########################################################################
 bitbake-layers show-layers | grep "meta-oe" > /dev/null
 layer_info=$?
 
@@ -33,6 +81,26 @@ if [ $layer_info -ne 0 ];then
 	bitbake-layers add-layer ../meta-openembedded/meta-oe
 else
 	echo "meta-oe layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-python" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+        echo "Adding meta-python layer"
+        bitbake-layers add-layer ../meta-openembedded/meta-python
+else
+        echo "meta-python layer already exists"
+fi
+
+bitbake-layers show-layers | grep "meta-networking" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+        echo "Adding meta-networking layer"
+        bitbake-layers add-layer ../meta-openembedded/meta-networking
+else
+        echo "meta-networking layer already exists"
 fi
 
 bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
