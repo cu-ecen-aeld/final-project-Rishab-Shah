4ab2dd2048da307fbb8c176fbe36130eb817e98e
Rishab-Shah <rishabshah1994@gmail.com>
2021-11-13 17:30:25 -0700

    updated script

--- build.sh
+++ build.sh
@@ -34,7 +34,6 @@ cat conf/local.conf | grep "${IMAGE_F}" > /dev/null
 local_imgf_info=$?
 
 # Add aesd-assignments package
-#CORE_IM_ADD="CORE_IMAGE_EXTRA_INSTALL += \"aesd-assignments i2c-config gpio-config\""
 CORE_IM_ADD="CORE_IMAGE_EXTRA_INSTALL += \"i2c-config gpio-config\""
 cat conf/local.conf | grep "${CORE_IM_ADD}" > /dev/null
 local_coreimadd_info=$?
@@ -49,14 +48,12 @@ AUTOLOAD_I2C="KERNEL_MODULE_AUTOLOAD:rpi += \"i2c-dev i2c-bcm2708\""
 cat conf/local.conf | grep "${AUTOLOAD_I2C}" > /dev/null
 local_i2c_autoload_info=$?
 
-
 ##########################################################################
 # Add if the support is missing in the local.conf file
 ##########################################################################
 if [ $local_conf_info -ne 0 ];then
 	echo "Append ${CONFLINE} in the local.conf file"
 	echo ${CONFLINE} >> conf/local.conf
-	
 else
 	echo "${CONFLINE} already exists in the local.conf file"
 fi
@@ -64,8 +61,7 @@ fi
 
 if [ $local_distro_info -ne 0 ];then
         echo "Append ${DISTRO_F} in the local.conf file"
-        echo ${DISTRO_F} >> conf/local.conf
-        
+        echo ${DISTRO_F} >> conf/local.conf 
 else
         echo "${DISTRO_F} already exists in the local.conf file"
 fi
@@ -73,7 +69,6 @@ fi
 if [ $local_imgadd_info -ne 0 ];then
         echo "Append ${IMAGE_ADD} in the local.conf file"
         echo ${IMAGE_ADD} >> conf/local.conf
-        
 else
         echo "${IMAGE_ADD} already exists in the local.conf file"
 fi
@@ -81,31 +76,27 @@ fi
 if [ $local_imgf_info -ne 0 ];then
         echo "Append ${IMAGE_F} in the local.conf file"
         echo ${IMAGE_F} >> conf/local.conf
-        
 else
         echo "${IMAGE_F} already exists in the local.conf file"
 fi
 
 if [ $local_coreimadd_info -ne 0 ];then
         echo "Append ${CORE_IM_ADD} in the local.conf file"
-        echo ${CORE_IM_ADD} >> conf/local.conf
-        
+        echo ${CORE_IM_ADD} >> conf/local.conf       
 else
         echo "${CORE_IM_ADD} already exists in the local.conf file"
 fi
 
 if [ $local_i2c_info -ne 0 ];then
-        echo "Append  ${MODULE_I2C} in the local.conf file"
+        echo "Append ${MODULE_I2C} in the local.conf file"
         echo ${MODULE_I2C} >> conf/local.conf
-
 else
         echo "${MODULE_I2C} already exists in the local.conf file"
 fi
 
 if [ $local_i2c_autoload_info -ne 0 ];then
-        echo "Append  ${AUTOLOAD_I2C} in the local.conf file"
+        echo "Append ${AUTOLOAD_I2C} in the local.conf file"
         echo ${AUTOLOAD_I2C} >> conf/local.conf
-
 else
         echo "${AUTOLOAD_I2C} already exists in the local.conf file"
 fi
@@ -154,16 +145,6 @@ else
         echo "meta-raspberrypi layer already exists"
 fi
 
-#bitbake-layers show-layers | grep "meta-aesd" > /dev/null
-#layer_info=$?
-
-#if [ $layer_info -ne 0 ];then
-#        echo "Adding meta-aesd layer"
-#        bitbake-layers add-layer ../meta-aesd
-#else
-#        echo "meta-aesd layer already exists"
-#fi
-
 bitbake-layers show-layers | grep "meta-i2c" > /dev/null
 layer_info=$?
 
@@ -183,6 +164,17 @@ if [ $layer_info -ne 0 ];then
 else
         echo "meta-gpio layer already exists"
 fi
+
+#bitbake-layers show-layers | grep "meta-server" > /dev/null
+#layer_info=$?
+
+#if [ $layer_info -ne 0 ];then
+#        echo "Adding meta-server layer"
+#        bitbake-layers add-layer ../meta-server
+#else
+#        echo "meta-server layer already exists"
+#fi
+
 
 ############################################################################
 set -e
